1adae390bd161713bec8c0ab32d2a458f483e30a
Nguyen Duc Phong <90232557+ChauBacHozz@users.noreply.github.com>
2024-08-21 01:08:37 +0700

    Delete stock_data_stream directory

--- stock_data_stream/mongodb_connector.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-# Assuming youre running mongod on 'localhost' with port 27017
-c = MongoClient("100.89.103.30:27017", 27017)
-# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-print(c.list_database_names())
--- stock_data_stream/src/data/loader/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cafe import DataLoaderCAFE
-from .cafe2 import DataLoaderCAFE2 
-from .vnd import DataLoaderVND
-
-__all__ = ['DataLoaderCAFE', 'DataLoaderVND', 'DataLoaderCAFE2']
